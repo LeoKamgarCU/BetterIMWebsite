13759865f9718477037e7d1e410d5d63c4bba82c
Leo Kamgar <leka5026@colorado.edu>
2022-11-12 13:17:44 -0700

    Teams listings page functionality added, and Default user1 profile inserted

--- Project-Code/src/init_data/insert.sql
+++ Project-Code/src/init_data/insert.sql
@@ -5,3 +5,14 @@ INSERT INTO sports (sportName) VALUES
 ('Soccer'),
 ('Football');
 
+
+-- Testing Teams
+-- password to type in login is 'asdf'
+INSERT INTO players (username, playerName, password, classYear, profilePhoto, joinDate) VALUES ('user1', 'leo', '$2b$10$pSWjpntEzHInJozarXe4Z..5GM5QeUE/z3OEfK3NHdDS4tjPSj4Yu', 2022, '', '2022-11-11');
+
+INSERT INTO teams (teamName) VALUES
+('Team 1'),
+('Team 2');
+
+INSERT INTO teamsToSports (teamID, sportID) VALUES (1, 3);
+INSERT INTO teamsToSports (teamID, sportID) VALUES (2, 3);
